c35d3010e1fbce86465aac77cf6336d1428f1724
Samuele Turci <samturci@google.com>
2019-07-31 15:39:24 +0200

    Add -no-review-update flag

--- cmd/reporter/reporter.go
+++ cmd/reporter/reporter.go
@@ -29,14 +29,15 @@ import (
 )
 
 var (
-	configFile = flag.String("config-file", "", "required report config file to use")
-	walkPath   = flag.String("walk-path", "", "path to search for Walks")
-	reviewFile = flag.String("review-file", "", "path to the file containing a list of last-known-good states - this needs to be writeable")
-	hostname   = flag.String("hostname", "", "host to review the differences for")
-	beforeFile = flag.String("before-file", "", "path to the file to compare against (last known good typically)")
-	afterFile  = flag.String("after-file", "", "path to the file to compare with the before state")
-	paginate   = flag.Bool("paginate", false, "pipe output into $PAGER in order to paginate and make reviews easier")
-	verbose    = flag.Bool("verbose", false, "print additional output for each file which changed")
+	configFile     = flag.String("config-file", "", "required report config file to use")
+	walkPath       = flag.String("walk-path", "", "path to search for Walks")
+	reviewFile     = flag.String("review-file", "", "path to the file containing a list of last-known-good states - this needs to be writeable")
+	hostname       = flag.String("hostname", "", "host to review the differences for")
+	beforeFile     = flag.String("before-file", "", "path to the file to compare against (last known good typically)")
+	afterFile      = flag.String("after-file", "", "path to the file to compare with the before state")
+	paginate       = flag.Bool("paginate", false, "pipe output into $PAGER in order to paginate and make reviews easier")
+	verbose        = flag.Bool("verbose", false, "print additional output for each file which changed")
+	noReviewUpdate = flag.Bool("no-review-update", false, "don't ask to update the \"last known good\" review")
 )
 
 const (
@@ -152,7 +153,7 @@ func main() {
 	}
 
 	// Update reviews file if desired.
-	if updateReviews() {
+	if !*noReviewUpdate && updateReviews() {
 		if err := rptr.UpdateReviewProto(ctx, after, *reviewFile); err != nil {
 			log.Fatal(err)
 		}
